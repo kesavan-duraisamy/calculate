f34e7c37d80d4ca54feb9c9fe577ac29e2f4ff0a
Kesavan D <93248720+kesavan-duraisamy@users.noreply.github.com>
2026-08-28 09:19:50 +0530

    Update Calculator.cs

--- src/Calculate/Calculator.cs
+++ src/Calculate/Calculator.cs
@@ -15,4 +15,25 @@ public class Calculator
         int value = a + b;
         return value;
     }
+
+    /// <summary>
+    /// Returns the difference of two integers (<paramref name="a"/> - <paramref name="b"/>).
+    /// </summary>
+    /// <param name="a">The minuend.</param>
+    /// <param name="b">The subtrahend.</param>
+    /// <returns>The result of <paramref name="a"/> - <paramref name="b"/>.</returns>
+    public int Subtract(int a, int b) {
+        int value = a - b;
+        return value;
+    }
+
+    public int Multiply(int a, int b) {
+        int value = a * b;
+        return value;
+    }
+
+    public int Divide(int a, int b) {
+        int value = a / b;
+        return value;
+    }
 }
